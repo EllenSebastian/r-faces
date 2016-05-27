8a367b8c6f0f77e248990fcbfb35526edca391a0
ellensebastian <ellenmseb@gmail.com>
2016-05-26 20:40:01 -0700

    add hilbert r implementation from https://github.com/mookerji/pl2

---
+++ hilbertr/python/setup.py
@@ -0,0 +1,21 @@
+ #! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = 'rtree',
+      version = '0.0.1',
+      description = 'Python RTree implementation',
+      author = 'Bhaskar Mookerji',
+      author_email = 'mookerji@gmail.com',
+      url = 'https://github.com/mookerji/pl2',
+      packages = ['rtree', 
+                  'rtree.rtree', 
+                  'rtree.types', 
+                  'rtree.utils', 
+                  'rtree.tests.test_bench',
+                  'rtree.tests.test_rtree',
+                  'rtree.tests.test_types',
+                  'rtree.tests.test_utils'],
+      classifiers = ["Development Status :: 2 - Pre-Alpha",
+                     "Environment :: Console"]
+)
