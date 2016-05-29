5cf39f976e4b24ada7307dfe2e45ccc5775b0871
ellensebastian <ellenmseb@gmail.com>
2016-05-29 01:17:39 -0700

    cleanup static hilbert rtree

--- static-hilbert-r/static_hilbert_tree.py
+++ static-hilbert-r/static_hilbert_tree.py
@@ -1,4 +1,17 @@
 import hilbert
+import math 
+
+
+def minimumSpanningRectangle(coords):
+	ll = list(coords[0])
+	ur = list(coords[0])
+	for coord in coords:
+		for i in range(len(coord)):
+			if coord[i] < ll[i]:
+				ll[i] = coord[i]
+			if coord[i] > ur[i]:
+				ur[i] = coord[i]
+	return (tuple(ll), tuple(ur))
 
 class LeafNode(object):
 	def __init__(self, keyCoords, keyIds = None, ll = None, ur = None,):
@@ -25,71 +38,39 @@ class InternalNode(object):
 		return '(' + ','.join([str(i) for i in self.ll]) + ')-(' + ','.join([str(i) for i in self.ur]) + ')'
 
 class StaticHilbertR(object):
-def __init__(self, coords, k):
-	hilbertnums = [] 
-	for coord in coords:
-		hilbertNum = hilbert.Hilbert_to_int(coord)
-		hilbertnums.append(hilbertNum, coord)
-	hilbertnums = sorted(hilbertnums)
+	def __init__(self, coords, k):
+		hilbertnums = sorted([[hilbert.Hilbert_to_int(coord), coord] for coord in coords])
+		# create leaf nodes
+		leafnodes = [] 
+		for leafnode in range(int(math.ceil(len(coords) / float(k)))):
+			curKeys = [i[1] for i in hilbertnums[k*leafnode:k*leafnode+k]]
+			node = LeafNode(curKeys)
+			leafnodes.append(node)
+		self.height = 1
+		# create higher level nodes
+		curLevelNodes = leafnodes
+		while len(curLevelNodes) > 1:
+			self.height += 1
+			nextLevelNodes = [] 
+			for nextLevelNode in range(int(math.ceil(len(curLevelNodes) / float(k)))):
+				curKeys = [i for i in curLevelNodes[k*nextLevelNode:k*nextLevelNode+k]]
+				node = InternalNode(curKeys)
+				nextLevelNodes.append(node)
+			curLevelNodes = nextLevelNodes
+		self.root = curLevelNodes[0]
+                
+	#prints trees of height 2 or 3
+	def printTree(self):
+		if self.height < 3:
+			print '        ' + self.root.toString()
+			if self.height == 2:
+				print '   '.join([i.toString() for i in self.root.children])
+		if self.height == 3:
+			print '                  ' + self.root.toString()
+			print '      ' + '             '.join([i.toString() for i in self.root.children])
+			print ' '.join([' '.join([i.toString() for i in j.children]) for j in self.root.children])
 
-
-def minimumSpanningRectangle(coords):
-	dim = len(coords[0])
-	ll = list(coords[0])
-	ur = list(coords[0])
-	for coord in coords:
-		for i in range(len(coord)):
-			if coord[i] < ll[i]:
-				ll[i] = coord[i]
-			if coord[i] > ur[i]:
-				ur[i] = coord[i]
-	return (tuple(ll), tuple(ur))
 
 coords = [(1,2,3),(2,3,4),(3,4,5),(5,6,7),(6,7,8),(7,8,9),(9,10,11),(10,11,12)]
-hilbertnums = [] 
-for coord in coords:
-	hilbertNum = hilbert.Hilbert_to_int(coord)
-	hilbertnums.append([hilbertNum, coord])
-
-hilbertnums = sorted(hilbertnums)
-# create leaf nodes
-leafnodes = [] 
-for leafnode in range(int(math.ceil(len(coords) / float(k)))):
-	curKeys = [i[1] for i in hilbertnums[k*leafnode:k*leafnode+k]]
-	node = LeafNode(curKeys)
-	leafnodes.append(node)
-
-# create higher level nodes
-cur_level_nodes = leafnodes
-while len(cur_level_nodes) > 1:
-	next_level_nodes = [] 
-	for nextLevelNode in range(int(math.ceil(len(cur_level_nodes) / float(k)))):
-		curKeys = [i for i in cur_level_nodes[k*nextLevelNode:k*nextLevelNode+k]]
-		node = InternalNode(curKeys)
-		next_level_nodes.append(node)
-	cur_level_nodes = next_level_nodes
-
-root = cur_level_nodes[0]
-printTree(root)
-
-def height(root):
-	height = 1
-	temp = root
-	while type(temp) == InternalNode:
-		height += 1
-		temp = temp.children[0]
-	return height
-
-#prints trees of height 2 or 3
-def printTree(root):
-	if height(root) < 3:
-		print '        ' + root.toString()
-		if height(root) == 2:
-			print '   '.join([i.toString() for i in root.children])
-	if height(root) == 3:
-		print '                  ' + root.toString()
-		print '      ' + '             '.join([i.toString() for i in root.children])
-		print ' '.join([' '.join([i.toString() for i in j.children]) for j in root.children])
-
-
-
+t = StaticHilbertR(coords, 2)
+t.printTree()
