439b857f1d335275ac7fd810edcd9319976ec072
ellensebastian <ellenmseb@gmail.com>
2016-05-29 04:13:54 -0700

    static hilbert tree cleanup

--- static-hilbert-r/static_hilbert_tree.py
+++ static-hilbert-r/static_hilbert_tree.py
@@ -19,29 +19,37 @@ def rectContainsPoint(rect, point):
 			return False
 	return True
 
-# rect1 = (l1, r1)
-# so l2.x = rect2[0][0]
-# l2.y = rect2[0][1]
-# r2.x = rect2[1][0]
-# r2.y = rect2[1][1]
 def rectsIntersect(rect1, rect2):
 	for i in range(len(rect1[0])):
 		if rect1[0][i] > rect2[1][i]: return False
 		if rect2[0][i] > rect1[1][i]: return False 
 	return True
- 
+
+# minmum distance btween a point an a rectangle as in 4.3.1 on p56
+def mindist(point, rect):
+	result = 0
+	for i in range(len(point)):
+		qi = point[i]
+		ri = qi
+		if qi < rect[0][i]: ri = rect[0][i]
+		elif qi > rect[1][i]: ri = rect[1][i]
+		result += (qi - ri)**2
+	return result
+
+# squared euclidean distance
+def dist(p1, p2):
+	return sum([(p1[i] - p2[i])**2 for i in range(len(p1))])
 
 class LeafNode(object):
 	def __init__(self, keyCoords, keyIds = None, ll = None, ur = None,):
 		self.keyIds = keyIds
 		self.keyCoords = keyCoords
 		if ll is None or ur is None:
-			self.ll, self.ur = minimumSpanningRectangle(keyCoords)
+			self.msr = minimumSpanningRectangle(keyCoords)
 		else:
-			self.ll = ll
-			self.ur = ur
+			self.msr = (ll, ur)
 	def toString(self):
-		return '(' + ','.join([str(i) for i in self.ll]) + ')-(' + ','.join([str(i) for i in self.ur]) + ')'
+		return '(' + ','.join([str(i) for i in self.msr[0]]) + ')-(' + ','.join([str(i) for i in self.msr[1]]) + ')'
 	def searchPoint(self, point):
 		return [i for i in self.keyCoords if point == i]
 	def searchRect(self, rect):
@@ -53,11 +61,11 @@ class InternalNode(object):
 		self.children = children
 		allcoords = []
 		for child in children:
-			allcoords.append(child.ll)
-			allcoords.append(child.ur)
-		self.ll, self.ur = minimumSpanningRectangle(allcoords)
+			allcoords.append(child.msr[0])
+			allcoords.append(child.msr[1])
+		self.msr = minimumSpanningRectangle(allcoords)
 	def toString(self):
-		return '(' + ','.join([str(i) for i in self.ll]) + ')-(' + ','.join([str(i) for i in self.ur]) + ')'
+		return '(' + ','.join([str(i) for i in self.msr[0]]) + ')-(' + ','.join([str(i) for i in self.msr[1]]) + ')'
 
 class StaticHilbertR(object):
 	def __init__(self, coords, k):
@@ -87,7 +95,7 @@ class StaticHilbertR(object):
 		else:
 			result = [] 
 			for child in curNode.children:
-				if rectContainsPoint((child.ll, child.ur), point):
+				if rectContainsPoint(child.msr, point):
 					result += self.pointSearchRec(point, child)
 		return result
     
@@ -97,17 +105,17 @@ class StaticHilbertR(object):
 		else:
 			result = [] 
 			for child in curNode.children:
-				if rectsIntersect((child.ll, child.ur), rect):
+				if rectsIntersect(child.msr, rect):
 					result += self.rangeSearchRec(rect, child)
 		return result
     
 	# return all entries that intersect with rect.
 	def rangeSearch(self, rect):
-		if rectsIntersect((self.root.ll, self.root.ur), rect):		
+		if rectsIntersect(self.root.msr, rect):		
 			return self.rangeSearchRec(rect, self.root)
     
 	def pointSearch(self, point):
-		if rectContainsPoint((self.root.ll, self.root.ur), point):
+		if rectContainsPoint(self.root.msr, point):
 			return self.pointSearchRec(point, self.root)
 		return []
         
@@ -124,7 +132,7 @@ class StaticHilbertR(object):
 					q.put(elem, dist(point, elem))
 			elif type(element) == InternalNode:
 				for child in element.children:
-					q.put(child, mindist(point, (child.ll, child.ur)))
+					q.put(child, mindist(point, child.msr))
 			else: # element is a (ll, ur)
 				curdist = dist(point, element)
 				curMinDist = mindist(point, q.queue[0]) if type(q.queue[0]) == InternalNode else dist(point, q.queue[0])
@@ -146,30 +154,12 @@ class StaticHilbertR(object):
 			print '      ' + '             '.join([i.toString() for i in self.root.children])
 			print ' '.join([' '.join([i.toString() for i in j.children]) for j in self.root.children])
 
-# TODO only search possible rects 
-
-# minmum distance btween a point an a rectangle as in 4.3.1 on p56
-def mindist(point, rectangle):
-	result = 0
-	for i in range(len(point)):
-		qi = point[i]
-		si = rectangle[0][i]
-		ti = rectangle[1][i]
-		ri = qi
-		if qi < si: ri = si
-		elif qi > ti: ri = ti
-		result += (qi - ri)**2
-	return result
-
-# squared euclidean distance
-def dist(p1, p2):
-	return sum([(p1[i] - p2[i])**2 for i in range(len(p1))])
 
 coords = [(1,2,3),(2,3,4),(3,4,5),(5,6,7),(6,7,8),(7,8,9),(9,10,11),(10,11,12)]
 t = StaticHilbertR(coords, 2)
 print t.kNearestNeighbors((5,6,7),2)
 print t.kNearestNeighbors((5,6,6),3)
-print t.kNearestNeighbors((1,2,3),4)
+print t.kNearestNeighbors((1,2,3),2)
 print t.rangeSearch(((1,2,3),(5,6,7)))
 print t.pointSearch((1,2,3))
 t.printTree()
