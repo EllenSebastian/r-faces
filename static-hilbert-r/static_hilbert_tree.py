7a22cbdcde45b140c2d90c56bbaadd10f53f08b1
ellensebastian <ellenmseb@gmail.com>
2016-05-29 02:49:05 -0700

    range and point queries in static_hilbert_tree.py

--- static-hilbert-r/static_hilbert_tree.py
+++ static-hilbert-r/static_hilbert_tree.py
@@ -13,6 +13,24 @@ def minimumSpanningRectangle(coords):
 				ur[i] = coord[i]
 	return (tuple(ll), tuple(ur))
 
+def rectContainsPoint(rect, point):
+	for i in range(len(point)):
+		if rect[0][i] > point[i] or rect[1][i] < point[i]:
+			return False
+	return True
+
+# rect1 = (l1, r1)
+# so l2.x = rect2[0][0]
+# l2.y = rect2[0][1]
+# r2.x = rect2[1][0]
+# r2.y = rect2[1][1]
+def rectsIntersect(rect1, rect2):
+	for i in range(len(rect1[0])):
+		if rect1[0][i] > rect2[1][i]: return False
+		if rect2[0][i] > rect1[1][i]: return False 
+	return True
+ 
+
 class LeafNode(object):
 	def __init__(self, keyCoords, keyIds = None, ll = None, ur = None,):
 		self.keyIds = keyIds
@@ -24,6 +42,10 @@ class LeafNode(object):
 			self.ur = ur
 	def toString(self):
 		return '(' + ','.join([str(i) for i in self.ll]) + ')-(' + ','.join([str(i) for i in self.ur]) + ')'
+	def searchPoint(self, point):
+		return [i for i in self.keyCoords if point == i]
+	def searchRect(self, rect):
+		return [i for i in self.keyCoords if rectContainsPoint(rect, i)]
 
 
 class InternalNode(object):
@@ -58,19 +80,53 @@ class StaticHilbertR(object):
 				nextLevelNodes.append(node)
 			curLevelNodes = nextLevelNodes
 		self.root = curLevelNodes[0]
-                
+    
+	def pointSearchRec(self, point, curNode):
+		if type(curNode) == LeafNode: # case where root is a leaf is already covered pointSearch
+			return curNode.searchPoint(point)
+		else:
+			result = [] 
+			for child in curNode.children:
+				if rectContainsPoint((child.ll, child.ur), point):
+					result += self.pointSearchRec(point, child)
+		return result
+    
+	def rangeSearchRec(self, rect, curNode):
+		if type(curNode) == LeafNode:
+			return curNode.searchRect(rect)
+		else:
+			result = [] 
+			for child in curNode.children:
+				if rectsIntersect((child.ll, child.ur), rect):
+					result += self.rangeSearchRec(rect, child)
+		return result
+    
+	# return all entries that intersect with rect.
+	def rangeSearch(self, rect):
+		if rectsIntersect((self.root.ll, self.root.ur), rect):		
+			return self.rangeSearchRec(rect, self.root)
+    
+	def pointSearch(self, point):
+		if rectContainsPoint((self.root.ll, self.root.ur), point):
+			return self.pointSearchRec(point, self.root)
+		return [] 
+    
 	#prints trees of height 2 or 3
 	def printTree(self):
 		if self.height < 3:
 			print '        ' + self.root.toString()
 			if self.height == 2:
 				print '   '.join([i.toString() for i in self.root.children])
-		if self.height == 3:
+		if self.height >= 3:
 			print '                  ' + self.root.toString()
 			print '      ' + '             '.join([i.toString() for i in self.root.children])
 			print ' '.join([' '.join([i.toString() for i in j.children]) for j in self.root.children])
 
+# TODO only search possible rects 
+
 
 coords = [(1,2,3),(2,3,4),(3,4,5),(5,6,7),(6,7,8),(7,8,9),(9,10,11),(10,11,12)]
 t = StaticHilbertR(coords, 2)
+print t.rangeSearch(((1,2,3),(5,6,7)))
+print t.pointSearch((1,2,3))
 t.printTree()
